9704ad727aeaf04ab55fabae754aab94353d9b32
hansen-kargo <hansen.dharmawan@kargo.tech>
2022-06-14 16:26:38 +0700

    Initial commit

---
+++ app/src/main/java/tech/kargo/displayoverapptest/MainActivity.kt
@@ -0,0 +1,53 @@
+package tech.kargo.displayoverapptest
+
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.provider.Settings
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        checkOverlayPermission()
+        println("starting service")
+        startService()
+    }
+
+    fun startService(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // check if the user has already granted
+            // the Draw over other apps permission
+            if (Settings.canDrawOverlays(this)) {
+                // start the service based on the android version
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    startForegroundService(Intent(this, ForegroundService::class.java))
+                } else {
+                    startService(Intent(this, ForegroundService::class.java))
+                }
+            }
+        } else {
+            startService(Intent(this, ForegroundService::class.java))
+        }
+    }
+
+    private fun checkOverlayPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.canDrawOverlays(this)) {
+                // send user to the device settings
+                val myIntent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION)
+                startActivity(myIntent)
+            }
+        }
+    }
+
+    // check for permission again when user grants it from
+    // the device settings, and start the service
+    override fun onResume() {
+        super.onResume()
+        startService()
+    }
+}
